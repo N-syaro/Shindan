a19cb03cd2c82271cb4c9aedbf342bf2ea1ae5a7
N-syaro <koya.syaro@gmail.com>
2026-03-12 16:29:56 +0900

    会話一文字送り実装

---
+++ Assets/TalkScripts/TalkDelay.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+
+public class TalkDelay : MonoBehaviour
+{
+    private bool isSkip = false;
+
+    public void SKip()//スキップフラグ
+    {
+        isSkip = true;
+    }
+    public  IEnumerator TextActive(Text text,string Data)//一文字ずつ流すコード
+    {
+        if(Data == null) { yield break;}
+        isSkip = false;
+        text.text = "";
+
+        for(var i = 0;i<Data.Length;i++)
+        {
+            if (isSkip)
+            {
+                text.text = Data;
+                break;
+            }
+            yield return new WaitForSeconds(0.1f);
+            text.text += Data[i];
+        }
+        yield return new WaitForSeconds(0.5f);
+    }
+}
