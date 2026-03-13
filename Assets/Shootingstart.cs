370be9234870aa3fe75beb51ded8d9c71cdb7cfe
内田涼介 <KTC24A32A0002@edu.kyoto-tech.ac.jp>
2026-03-13 16:28:26 +0900

    最初の段階

---
+++ Assets/Shootingstart.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections;
+using Unity.VisualScripting;
+
+
+
+public class Shootingstart : MonoBehaviour
+{
+    private RectTransform rectTransform;
+    public float duration = 2.0f;
+
+
+    private bool isMoving = false;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    
+    public void TriggerFalling()
+    {
+        
+        if (!isMoving)
+        {
+            StartCoroutine(Shootingdown());
+        }
+    }
+
+    IEnumerator Shootingdown()
+    {
+        yield return new WaitForSeconds(2.0f);
+
+        Vector2 startPos = new Vector2(rectTransform.anchoredPosition.x, 1000);
+        Vector2 endPos = new Vector2(rectTransform.anchoredPosition.x, 0f);
+
+        float elapsed = 0;
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / duration;
+
+            rectTransform.anchoredPosition = Vector2.Lerp(startPos, endPos, t);
+
+            yield return null;
+        }
+
+        rectTransform.anchoredPosition = endPos;
+        isMoving = false;
+    }
+}
