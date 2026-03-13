856031d1c713abfd38c6658f281050faf7bb4211
gyakuhannta <ktc24a31a0039>
2026-03-13 13:57:01 +0900

    プレイヤー操作移動から弾丸の発射まで完成

--- Assets/preya/Preya_min.cs
+++ Assets/preya/Preya_min.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
@@ -6,28 +7,55 @@ public class Preya_min : MonoBehaviour
 
     public bool point = true;//マウスでのキャラ操作の切り替え
     public float sped=10f;//移動速度
+    public float dstm=5f; //弾丸消滅速度
+    public float brsp = 1000f;//弾丸の速度
     Vector2 mousePos;
     Vector2 mouseworldPos;//マウスポインタ位置
-    public GameObject barte1;
-    public GameObject barte2;
-    public GameObject barte3;
-    public GameObject barte4;
-    private float wh;
-
- 
+    public GameObject[] bart;//弾丸のプレハブ
+    private int balet ;//弾丸の種類
+    private float wh;//マウスホイールの数値
+    
     // Update is called once per frame
     void Update()
     {
         //マウスポインタの変換
         mousePos = Input.mousePosition;
         mouseworldPos = Camera.main.ScreenToWorldPoint(mousePos);
-
         
+        wh = Input.mouseScrollDelta.y;//マウスホイール取得
+       if (wh !=0)//弾丸選択
+       {    //マウスホイールの移動
+            if (wh > 0) 
+            {
+                balet++;
+            }
+            if (wh < 0) 
+            {
+                balet--;
+            }
+            //範囲の指定
+            if(balet >= bart.Length) 
+            {
+                balet = 0;
+            }else if (balet < 0)
+            {
+                balet = bart.Length-1;
+            }
 
-        if(point== true)//キャラ操作用
+            Debug.Log(balet);
+
+       }
+  
+
+       
+        if (point== true)//キャラ操作用
         {//マウスの位置へ向けて移動する
-            transform.position = Vector2.MoveTowards(transform.position, mouseworldPos, sped * Time.deltaTime);  
-
+            transform.position = Vector2.MoveTowards(transform.position, mouseworldPos, sped * Time.deltaTime);
+            //弾丸の生成
+            if (Input.GetMouseButtonDown(0)) 
+            {
+                Shot();
+            }
 
 
 
@@ -35,10 +63,13 @@ public class Preya_min : MonoBehaviour
 
         
     }
-    private void FixedUpdate()
-    {
-        Input.GetAxis("Mouse ScrollWheel");
-
+    void Shot() 
+    {//弾丸の発射処理
+        GameObject newbalet = Instantiate(bart[balet],this.transform.position,Quaternion.identity);
+        Rigidbody2D bllet2d= newbalet.GetComponent<Rigidbody2D>();
+        bllet2d.AddForce(this.transform.up* brsp);
+        Destroy(newbalet, dstm);
 
     }
+
 }
