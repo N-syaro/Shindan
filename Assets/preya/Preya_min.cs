e26945fd51f7057edbdeb19ed0792ecf067a5899
gyakuhannta <ktc24a31a0039>
2026-03-13 14:47:54 +0900

    発射クールタイム追加

--- Assets/preya/Preya_min.cs
+++ Assets/preya/Preya_min.cs
@@ -4,17 +4,23 @@ using UnityEngine.Rendering.Universal;
 
 public class Preya_min : MonoBehaviour
 {
-
+    Vector2 mousePos;
+    Vector2 mouseworldPos;//マウスポインタ位置
     public bool point = true;//マウスでのキャラ操作の切り替え
     public float sped=10f;//移動速度
     public float dstm=5f; //弾丸消滅速度
     public float brsp = 1000f;//弾丸の速度
-    Vector2 mousePos;
-    Vector2 mouseworldPos;//マウスポインタ位置
+    public float dlitm=5f; //coolタイム
     public GameObject[] bart;//弾丸のプレハブ
+
     private int balet ;//弾丸の種類
     private float wh;//マウスホイールの数値
-    
+    private bool faia=true;
+    float taime = 0;//タイマー用の関数
+    private void Start()
+    {
+       
+    }
     // Update is called once per frame
     void Update()
     {
@@ -48,17 +54,33 @@ public class Preya_min : MonoBehaviour
   
 
        
-        if (point== true)//キャラ操作用
+        if (point)//キャラ操作用
         {//マウスの位置へ向けて移動する
             transform.position = Vector2.MoveTowards(transform.position, mouseworldPos, sped * Time.deltaTime);
-            //弾丸の生成
-            if (Input.GetMouseButtonDown(0)) 
+          
+            taime += Time.deltaTime;
+            if(Input.GetMouseButtonDown(0))
             {
-                Shot();
+                if(faia)
+                {
+                    Debug.Log("弾を打ちました");
+                    Shot();
+                    faia = false;
+                    taime = 0;
+                    Debug.Log(taime);
+                }
+                else
+                {
+                    Debug.Log("cool中");
+                   
+                    if (taime >= dlitm)
+                    {
+                        faia = true;
+                        taime = 0;
+                        Debug.Log("弾が打てます");
+                    }
+                }
             }
-
-
-
         }
 
         
