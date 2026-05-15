d2aad509d4b3427e3e38f1dc60003e81dfc9f1e3
gyakuhannta <ktc24a31a0039@edu.kyoto-tech.ac.jp>
2026-05-15 17:37:06 +0900

    シューティングパートクリアフラグ追加他生成関連のバグ修正

--- Assets/purehavu/enemy/enmiyspon/enemy_sprn.cs
+++ Assets/purehavu/enemy/enmiyspon/enemy_sprn.cs
@@ -6,18 +6,35 @@ public class enemy_sprn : MonoBehaviour
 
     public Vector2[] weipos;//移動の目標地点
     public float mube=5f;//移動時間
+    public bool culafl=false;//クリアフラグ
     int pint=0;//移動ポイントの数
+    [SerializeField] bool next=false;//クリア用オブジェクトはtrue
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)//当たり判定
+    {
+        if (next!)
+        {
+            if (collision.gameObject.tag == "ballt")//balltタグに当たったらクリアフラグを立てる
+            {
+                culafl = true;
+                Debug.Log(culafl);
+
+            }
+
+        }
+
+
+    }
     void Update()
     {
 
         if (weipos==null||weipos.Length==0) return;//未設定の際の処理
-      //ウェイポイントへ向けて移動する
-         Vector2 taget = weipos[pint];
 
-        transform.position=Vector2.MoveTowards(transform.position, taget, mube * Time.deltaTime);
+         //ウェイポイントへ向けて移動する
+         Vector2 taget = weipos[pint];
+         transform.position=Vector2.MoveTowards(transform.position, taget, mube * Time.deltaTime);
 
         if(Vector2.Distance(transform.position, taget)<0.001f)//移動した後のポイント更新
         {
@@ -28,5 +45,9 @@ public class enemy_sprn : MonoBehaviour
             }
         }
         
+        
     }
+   
+
+
 }
