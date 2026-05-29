0cb64c6a85eedcea7cb2ca861a44e78b4ef9191e
gyakuhannta <ktc24a31a0039@edu.kyoto-tech.ac.jp>
2026-05-29 19:43:45 +0900

    文字に当たった判定と当たった時に少しだけ無敵になる処理が完成次は当たったらダメージエフェクト処理を作りたい

--- Assets/purehavu/preya/Enm.cs
+++ Assets/purehavu/preya/Enm.cs
@@ -5,10 +5,16 @@ public class Enm : MonoBehaviour
 {
     //プレイヤーの効果処理のスクリプト
 
-   public bool timeon = false;//タイマースタート
-    public float timelemt = 50f;//時間制限
+  [SerializeField] bool timeon = false;//タイマースタート
+   [SerializeField] float timelemt = 50f;//時間制限
+    [SerializeField] float damegerup=3f;
+    private BoxCollider2D cpplid;
     float taima = 0f;//タイマー
     float taima2 = 0f;//タイマーダメージ
+    bool hit=false;
+    float damgct;
+
+
    
     public float demegte = 1f;//ダメージで減らす時間
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -16,6 +22,8 @@ public class Enm : MonoBehaviour
     {//初期化
          
         taima = 0f;
+        damgct = 0f;
+        cpplid =  GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -34,17 +42,33 @@ public class Enm : MonoBehaviour
 
           
         }
-    }
-
-   
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        //Enemyに当たったら時間-1
-        if (collision.gameObject.tag == "Enemy") 
+        if (hit == true) 
         {
-            Debug.Log("dameg");
-            taima2 =+ demegte;
+            damgct += Time.deltaTime;
+            if (damegerup < damgct) 
+            {
+                hit = false;
+                damgct = 0f;
+                Debug.Log(hit);
+            }
 
         }
+
+     
     }
+
+  
+   
+   
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+       if (collision.gameObject.tag == "Enemy"&&hit ==false)
+       {
+            hit = true;
+            Debug.Log(hit);
+            
+        }
+    }
+    
 }
