ccf51b91598b66bcdec8e7bd14e53ffb9a44eec7
GTUNE2-01\ktc <>
2026-06-01 19:47:35 +0900

    ぱねるUI追加

--- Assets/gemu.taim/gemu_obuge.cs
+++ Assets/gemu.taim/gemu_obuge.cs
@@ -1,19 +1,28 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class gemu_obuge : MonoBehaviour
 {
     //敵の生成処理のスクリプト
-   
+    [SerializeField]
+    GameManager gameManager;
     public bool stat=true;
     public GameObject[] enemiobuject;//生成するゲームオブジェクト
     public float[] spulnt;//生成するタイミング
+    private string SceneName;
     float taimudl;//経過時間
     int next = 0;//生成する数
     bool onstop;
+
+    /*
+     * 生成しきったらgamemangerのExp_endをtrueにする処理を入れたい
+     * 
+     *
+     */
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        SceneName = SceneManager.GetActiveScene().name;
 
     }
 
@@ -23,38 +32,17 @@ public class gemu_obuge : MonoBehaviour
     void Update()
     {
         if (enemiobuject==null)return;
-
-
-
-       
-
-
-        if (stat == true)
-        {
-            
             taimudl += Time.deltaTime;
-            //タイマー
+        //タイマー
+        if (next < taimudl && next < spulnt.Length)//順次生成処理
+        {
 
-           
-
-         
-         
-            if (next < taimudl&& next<spulnt.Length)//順次生成処理
+            if (taimudl > spulnt[next])
             {
-
-                if (taimudl  >spulnt[next])
-                {
-                    sponw();
-                    next++;
-                }
+                sponw();
+                next++;
             }
-
-           
         }
-
-
-
-
     }
     void sponw()//生成処理
     {
