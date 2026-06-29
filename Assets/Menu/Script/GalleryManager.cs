a3c598c40ecc979524f49a3c43495f0efc9d33c1
ktc <>
2026-06-29 19:44:57 +0900

    ギャラリー完成（仮）

--- Assets/Menu/Script/GalleryManager.cs
+++ Assets/Menu/Script/GalleryManager.cs
@@ -1,31 +1,73 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GalleryManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    [SerializeField] GameObject showCanvas;
+
+    [SerializeField] GameObject checkCanvas;
+
+    [SerializeField] Image showImage;
+
+
+    string Title = "Title";
+
+    public FadeOutIn fadeout;
+
+    IEnumerator Start()
     {
-        
+        showCanvas.SetActive(false);
+        checkCanvas.gameObject.SetActive(true);
+
+        //1フレーム待つ(消えてしまうオブジェクトを参照しないようにするため) 
+        yield return null;
+
+
+        fadeout = FindFirstObjectByType<FadeOutIn>();
+
     }
+    
 
-    // Update is called once per frame
-    void Update()
+
+    public void ShowButton(Sprite sprite)
     {
+        showCanvas.SetActive(true);
+
+        showImage.sprite = sprite;
         
-    }
-
-    public void ShowButton(Image image)
-    {
-
-
     }
 
     public void ReturnButton()
     {
+        showCanvas.SetActive(false);
 
     }
 
+    public void OKButton()
+    {
+        checkCanvas.gameObject.SetActive(false);
+    }
+
+    public void NOButton()
+    {
+
+
+        if (fadeout != null)
+        {
+            fadeout.fadeOutIn(0f, 0.2f, 0.2f);
+        }
+        else
+        {
+            Debug.LogError("フェードオブジェクトがない");
+        }
+
+        //タイトル画面に戻る
+        SceneManager.LoadScene(Title);
+
+    }
 
 
 }
